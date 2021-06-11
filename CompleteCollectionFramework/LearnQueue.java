e64183aac1d475b0fc95e15c2cc0f157885e8613
DhruvilSoni <dhruvilsoni1703@gmail.com>
2021-06-12 00:30:15 +0530

    Add files via upload

---
+++ CompleteCollectionFramework/LearnQueue.java
@@ -0,0 +1,14 @@
+package CompleteCollectionFramweork;
+
+import java.util.*;
+
+public class LearnQueue {
+
+		public static void main(String[] args) {
+				
+				//Queue - First In First Out (FIFO)
+				
+				
+
+		}
+}
